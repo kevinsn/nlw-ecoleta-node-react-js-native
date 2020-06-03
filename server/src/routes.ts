5e0f0d7a2fbaf7ff729c901913ff38246ed91055
Kevin <kevinsadao@hotmail.com>
2020-06-03 01:37:16 -0300

    Pontos de coleta

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -8,12 +8,40 @@ routes.get('/items', async (request,response) =>{
 
     const serializedItems = items.map(item => {
         return {
+            id: item.id,
             name: item.title,
             image_url: `http://localhost:3333/uploads/${item.image}`,
         };
     });
 
     return response.json(serializedItems);
+});
+
+routes.post('/points', async (request,response) =>{
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    await knex('points').insert({
+        image: 'image-placeholder',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    });
+
+    return response.json({success: true});
+
 });
 
 export default routes;
