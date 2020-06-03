009bdaa3264bddfd8f9bc5d3dec3b6e5d8e307de
Kevin <kevinsadao@hotmail.com>
2020-06-03 18:43:26 -0300

    Implementação de estado

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import Header from './Header';
@@ -7,10 +7,17 @@ import Header from './Header';
 // Exemplo: se no return do App() tivesse "return <h1>Ecolete<h1>;"
 
 function App() {
+  const [counter, setCounter] = useState(0); // useState = Retorn um array [valor do estado, função para atualizar o valor do estado]
+  function handleButtonClick(){
+    setCounter (counter + 1);
+  }
+  
   return ( 
     <div>
-      <Header title= "Hello world"/>      
+      <Header title= {`Contador: ${counter}`}/>      
       <h1>Conteúdo da aplicação</h1>
+      <h1>{counter}</h1>
+      <button type="button" onClick={handleButtonClick}>Increment</button>
     </div>
   );
 }
