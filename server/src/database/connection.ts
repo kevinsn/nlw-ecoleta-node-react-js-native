6e81f3aed3c5567a18e63103310befa24528e89f
Kevin <kevinsadao@hotmail.com>
2020-06-03 00:41:42 -0300

    Criação das tabelas points, items e point_items

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -2,12 +2,14 @@ import knex from 'knex';
 import path from 'path';
 
 // __dirname = retorna o caminho onde está sendo executado
+// Migrations = histórico do banco de dados
 
 const connection = knex({
     client: 'sqlite3',
     connection: {
         filename: path.resolve(__dirname, 'database.sqlite'),
-    }
+    },
+    useNullAsDefault: true,
 });
 
 export default connection;
