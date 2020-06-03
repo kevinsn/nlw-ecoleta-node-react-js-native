05bb6dc9aa7bc74d9a7f32ba88df90d29f7606a6
Kevin <kevinsadao@hotmail.com>
2020-06-03 18:17:44 -0300

    Adicionado primeiro componente (header)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import Header from './Header';
+
+// JSX = sintaxe de XML dentro do JavaScript
+// Exemplo: se no return do App() tivesse "return <h1>Ecolete<h1>;"
+
 function App() {
-  return (
-    <div id="app">
-      <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
+  return ( 
+    <div>
+      <Header/>      
+      <h1>Conteúdo da aplicação</h1>
     </div>
   );
 }
