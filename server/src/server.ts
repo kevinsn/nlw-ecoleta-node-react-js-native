586287c9b690948feb5f4cad8998254e06260050
Kevin <kevinsadao@hotmail.com>
2020-06-03 02:45:47 -0300

    Import cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,11 @@
 import express, { application } from 'express';
+import cors from 'cors';
 import path from 'path';
 import routes from './routes';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
