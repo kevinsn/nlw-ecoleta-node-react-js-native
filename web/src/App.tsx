100fad51db2a6119c40d26de3a570f283372c17d
Kevin <kevinsadao@hotmail.com>
2020-06-03 23:39:34 -0300

    Página inicial e estilos

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,34 @@
-import React, { useState } from 'react';
-import './App.css';
-
-import Header from './Header';
+// import React, { useState } from 'react';
+// import Header from './Header';
 
 // JSX = sintaxe de XML dentro do JavaScript
 // Exemplo: se no return do App() tivesse "return <h1>Ecolete<h1>;"
 
-function App() {
-  const [counter, setCounter] = useState(0); // useState = Retorn um array [valor do estado, função para atualizar o valor do estado]
+/*
+Dentro do App():
+
+const [counter, setCounter] = useState(0); // useState = Retorn um array [valor do estado, função para atualizar o valor do estado]
   function handleButtonClick(){
     setCounter (counter + 1);
   }
-  
+Return do App():
+
+<div>
+  <Header title= {`Contador: ${counter}`}/>      
+  <h1>Conteúdo da aplicação</h1>
+  <h1>{counter}</h1>
+  <button type="button" onClick={handleButtonClick}>Increment</button>
+</div>
+*/
+
+import React from 'react';
+import './App.css';
+
+import Home from './pages/Home';
+
+function App() {  
   return ( 
-    <div>
-      <Header title= {`Contador: ${counter}`}/>      
-      <h1>Conteúdo da aplicação</h1>
-      <h1>{counter}</h1>
-      <button type="button" onClick={handleButtonClick}>Increment</button>
-    </div>
+    <Home />
   );
 }
 
