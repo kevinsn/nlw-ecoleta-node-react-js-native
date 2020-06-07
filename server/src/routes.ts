f48174d6763173f2fc9cb70a3f368dc8ff9dc56a
Kevin <kevinsadao@hotmail.com>
2020-06-07 00:39:37 -0300

    Validação dos dados

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import knex from './database/connection';
 import multer from 'multer';
 import multerConfig from './config/multer';
+import { celebrate, Joi } from 'celebrate';
 
 // index (listagem)
 // show (único registro)
@@ -22,6 +23,25 @@ routes.get('/items', itemsController.index);
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
 
-routes.post('/points', upload.single('image'),pointsController.create);
+routes.post(
+    '/points', 
+    upload.single('image'), 
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.string().required(),
+            latitude: Joi.string().required(),
+            longitude: Joi.string().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required()
+        })
+    },{
+        abortEarly: false,
+
+    }),
+    pointsController.create
+);
 
 export default routes;
